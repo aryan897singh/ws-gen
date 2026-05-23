cf55901b1d973ba77883cbe65f7a3e85e7a17024
Aryan Singh <aryan897work@gmail.com>
2026-05-22 21:57:56 -0700

    test(AddTopicForm): verify invalid(duplicate, case insensitive) input submission
    Refs: Issue #1 Screen-1/Adding/Spec-3

--- mobile/src/components/AddTopicForm.test.tsx
+++ mobile/src/components/AddTopicForm.test.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 import AddTopicForm from './AddTopicForm';
 
+const existingTopics = ['Gradient', 'SGD', 'Infinite Series', 'Angular Momentum'];
+
 describe('AddTopicForm Component', () => {
     //Spec 4a. Adding the topic
     it('successfully adds a valid topic', () =>{
         const mockOnAddTopic = jest.fn(); 
         const {getByPlaceholderText, getByText} = render(
-            <AddTopicForm onAddTopic={mockOnAddTopic}/>
+            <AddTopicForm onAddTopic={mockOnAddTopic} addTopicProps={existingTopics} />
         );
 
         const input = getByPlaceholderText('Enter a topic: ');
@@ -25,7 +27,7 @@ describe('AddTopicForm Component', () => {
     it('succesfully prevents topic name > 50 characters', () => {
         const mockOnAddTopic = jest.fn(); //mock func
         const {getByPlaceholderText, getByText} = render (
-            <AddTopicForm onAddTopic={mockOnAddTopic}/>
+            <AddTopicForm onAddTopic={mockOnAddTopic} addTopicProps={existingTopics}/>
         );
 
         //Fake rendering of screen, now scan and fire:
@@ -44,7 +46,7 @@ describe('AddTopicForm Component', () => {
 
         //now render the screen to test
         const {getByPlaceholderText, getByText} = render (
-            <AddTopicForm onAddTopic={mockOnAddTopic}/>
+            <AddTopicForm onAddTopic={mockOnAddTopic} addTopicProps={existingTopics}/>
         );
 
         //Now scan for inputs and try to input
@@ -58,6 +60,28 @@ describe('AddTopicForm Component', () => {
         expect(addButton.props.disabled).toBe(true);
     })
 
+    //Spec 3a. - Refuse to add already existing topics 
+    it('successfuly refuses to add an existing topic regardless of case', () =>{
+        //mock function:
+        const mockOnAddTopic = jest.fn();
 
-    
+        //render the screen:
+        const {getByPlaceholderText, getByText} = render(
+            <AddTopicForm onAddTopic={mockOnAddTopic} addTopicProps={existingTopics}/>
+        )
+
+        //Now scan the screen, simulate the input 
+        const input = getByPlaceholderText('Enter a topic: ');
+        const addButton = getByText('add');
+
+        fireEvent.changeText(input, 'sgD');
+        expect(addButton.props.disabled).toBe(true);
+        expect(getByText('Topic Already Exists!')).toBeTruthy();
+
+        fireEvent.changeText(input, 'aNgUlar MomenTUM');
+        expect(addButton.props.disabled).toBe(true);
+        expect(getByText('Topic Already Exists!')).toBeTruthy();
+    });
+
+
 });
