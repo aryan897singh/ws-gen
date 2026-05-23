95b56998e9a814b9670b424831e79c38e242f065
Aryan Singh <aryan897work@gmail.com>
2026-05-22 19:46:53 -0700

    test(AddTopicForm): verify maximum char submission
    Refs: Issue #1. Screen-1/Adding/Spec-2

--- mobile/src/components/AddTopicForm.test.tsx
+++ mobile/src/components/AddTopicForm.test.tsx
@@ -1,10 +1,9 @@
-//Generating test cases for Screen 1: Adding, Spec 4:
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 import AddTopicForm from './AddTopicForm';
 
 describe('AddTopicForm Component', () => {
-    //Test 4. Adding the topic
+    //Spec 4a. Adding the topic
     it('successfully adds a valid topic', () =>{
         const mockOnAddTopic = jest.fn(); 
         const {getByPlaceholderText, getByText} = render(
@@ -20,6 +19,22 @@ describe('AddTopicForm Component', () => {
         expect(mockOnAddTopic).toHaveBeenCalledWith('Calculus III');
         expect(mockOnAddTopic).toHaveBeenCalledTimes(1);
         
+    });
+
+    //Spec 2a. 50 Limit Char
+    it('succesfully prevents topic name > 50 characters', () => {
+        const mockOnAddTopic = jest.fn(); //mock func
+        const {getByPlaceholderText, getByText} = render (
+            <AddTopicForm onAddTopic={mockOnAddTopic}/>
+        );
+
+        //Fake rendering of screen, now scan and fire:
+        const input = getByPlaceholderText('Enter a topic: ');
+        const addButton = getByText('add');
+
+        fireEvent.changeText(input, '"The quick brown fox jumps over the lazy dog\'s head.'); //51 characters
+
+        expect(addButton.props.disabled).toBe(true); //Checking if the button is disabled 
     });
 
 
