2fc692daa9d78c096920853abdbe850be779c334
Aryan Singh <aryan897work@gmail.com>
2026-05-22 20:22:46 -0700

    feat(AddTopicForm): add addTopicProps to component contract

--- mobile/src/components/AddTopicForm.tsx
+++ mobile/src/components/AddTopicForm.tsx
@@ -3,8 +3,9 @@ import { View } from 'react-native';
 
 interface AddTopicFormProps {
     onAddTopic: (topic: string) => void;
+    addTopicProps: string[];
 }
 
-export default function AddTopicForm({onAddTopic} : AddTopicFormProps) {
+export default function AddTopicForm({onAddTopic, addTopicProps} : AddTopicFormProps) {
   return <View />;
 }
