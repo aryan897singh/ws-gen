86758d1e214695d4fc161898b69f22f55348ee74
Aryan Singh <aryan897work@gmail.com>
2026-05-22 18:28:58 -0700

    feat: define acceptance parameter for AddTopicForm component

--- mobile/src/components/AddTopicForm.tsx
+++ mobile/src/components/AddTopicForm.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 
-export default function AddTopicForm() {
+interface AddTopicFormProps {
+    onAddTopic: (topic: string) => void;
+}
+
+export default function AddTopicForm({onAddTopic} : AddTopicFormProps) {
   return <View />;
 }
