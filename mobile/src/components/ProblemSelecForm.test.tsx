3a8e23e954e2c5d313d6574112639cea94a115c8
Aryan Singh <aryan897work@gmail.com>
2026-05-23 22:42:03 -0700

    test(ProblemSelecForm): verify max problem input
    Refs Issue #1 Component-2/Spec-2

--- mobile/src/components/ProblemSelecForm.test.tsx
+++ mobile/src/components/ProblemSelecForm.test.tsx
@@ -3,7 +3,7 @@ import { render, fireEvent } from '@testing-library/react-native';
 import ProblemSelecForm from './ProblemSelecForm';
 
 const GEN_BTN_TXT = 'Generate Question Set';
-const TOTAL_PROB_SLIDER_ID = 'Total Problems'; 
+const TOTAL_PROB_SLIDER_ID = 'total-problems-slider'; 
 const CHOCLATE_PROB_INPUT = 'Chocolate Problems';
 
 describe('ProblemSelecForm Component', () => {
@@ -17,7 +17,7 @@ describe('ProblemSelecForm Component', () => {
         jest.clearAllMocks();
     });
 
-    it('Accepts a minimum of 1 total question', () => {
+    it('accepts a minimum of 1 total question', () => {
         //render screen
         const {getByTestId, getByText} = render(
             <ProblemSelecForm onGenerate={mockOnGenerate}/>
@@ -25,6 +25,18 @@ describe('ProblemSelecForm Component', () => {
         //scan and test 
         const slider = getByTestId(TOTAL_PROB_SLIDER_ID);
         fireEvent(slider, 'onValueChange', 0);
+
+        const genButton = getByText(GEN_BTN_TXT);
+        expect(genButton.props.disabled).toBe(true);
+    })
+
+    it('accepts a maximum of 20 total questions', () => {
+        const {getByTestId, getByText} = render(
+            <ProblemSelecForm onGenerate={mockOnGenerate}/>
+        )
+
+        const slider = getByTestId(TOTAL_PROB_SLIDER_ID);
+        fireEvent(slider, 'onValueChange', 21);
 
         const genButton = getByText(GEN_BTN_TXT);
         expect(genButton.props.disabled).toBe(true);
