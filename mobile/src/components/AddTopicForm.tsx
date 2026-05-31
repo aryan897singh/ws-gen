7718e6d66bdb7647ef3415cfd72f7946a304e127
Aryan Singh <aryan897work@gmail.com>
2026-05-31 10:50:57 -0700

    ci(mobile): install prettier and configure formatting scripts

--- mobile/src/components/AddTopicForm.tsx
+++ mobile/src/components/AddTopicForm.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
 interface AddTopicFormProps {
-    onAddTopic: (topic: string) => void;
-    addTopicProps: string[];
+  onAddTopic: (topic: string) => void;
+  addTopicProps: string[];
 }
 
-export default function AddTopicForm({onAddTopic, addTopicProps} : AddTopicFormProps) {
+export default function AddTopicForm({
+  onAddTopic,
+  addTopicProps,
+}: AddTopicFormProps) {
   return <View />;
 }
--- mobile/src/components/ProblemSelecForm.tsx
+++ mobile/src/components/ProblemSelecForm.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
 /*This 'dto' needs to be sent to backend for processing
 hence the screen holding this component must hold this in it's state */
-export interface ProblemSettings { 
+export interface ProblemSettings {
   totalProblems: number;
   chocolateProblems: number;
   prompt: string;
@@ -13,6 +13,8 @@ interface ProblemSelecFormProps {
   onGenerate: (settings: ProblemSettings) => void;
 }
 
-export default function ProblemSelecForm({ onGenerate }: ProblemSelecFormProps) {
+export default function ProblemSelecForm({
+  onGenerate,
+}: ProblemSelecFormProps) {
   return <View />;
 }
